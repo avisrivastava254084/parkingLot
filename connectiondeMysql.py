c7348a43e4df3dc888954d343c913c5b921bfe50
aviral srivastava <aviral.srivastava@wingify.com>
2018-03-28 22:35:05 +0530

    sets up the configs and the database.

---
+++ connectiondeMysql.py
@@ -0,0 +1,3 @@
+import MySQLdb
+
+db = MySQLdb.connect(host="localhost", user="root", passwd="indicadls02", db="parking_lot") 
---
+++ tickets.py
@@ -0,0 +1,17 @@
+class tickets:
+	def __init__(self, maximumSlots, carNumber, carColor):
+		self.maximumSlots = maximumSlots
+		self.carNumber = carNumber
+		self.carColor = carColor
+
+	def generateTicket(self):
+		if(checkSlot):
+			slot = self.nearestSlot()
+			return slot
+		else:
+			return ("Sorry, no slots left")
+
+	def checkSlot(self):
+		print("run the sql query here for checking the slots")
+		
+
