42a7dae7b60c9912078e14220bd7a3dfd2764d9e
Aviral Srivastava <avi.srivastava254084@gmail.com>
2018-04-02 00:35:31 +0530

    Update connectiondeMysql.py

--- connectiondeMysql.py
+++ connectiondeMysql.py
@@ -1,7 +1,7 @@
 import MySQLdb
 
 class DBConnection:
-	db = MySQLdb.connect(host="localhost", user="root", passwd="indicadls02", db="parking_lot")
+	db = MySQLdb.connect(host="localhost", user="root", passwd="password", db="parking_lot")
 
 	def getDBConnection(self):
 		print "Attempting to connect to DB"
